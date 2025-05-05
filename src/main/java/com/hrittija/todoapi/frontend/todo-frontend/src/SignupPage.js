53487807adb3925f778368335a06f81de9c9753f
hrittijab <hrittija2001@gmail.com>
2025-05-05 12:06:24 -0600

    Added password strength feature

--- src/main/java/com/hrittija/todoapi/frontend/todo-frontend/src/SignupPage.js
+++ src/main/java/com/hrittija/todoapi/frontend/todo-frontend/src/SignupPage.js
@@ -13,6 +13,7 @@ function SignUpPage() {
     let strength = 0;
     if (password.length >= 8) strength++;
     if (/[A-Z]/.test(password)) strength++;
+    if (/[a-z]/.test(password)) strength++;
     if (/[0-9]/.test(password)) strength++;
     if (/[@$!%*?&]/.test(password)) strength++;
     return strength;
